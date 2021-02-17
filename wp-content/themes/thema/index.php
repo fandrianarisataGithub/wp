a0782b2d9e943d081135bdeae1c752900a561129
Fenitriniaina <fandrianarisata2@gmail.com>
2021-02-17 16:56:22 +0300

    debut 17-02-2021

---
+++ wp-content/themes/thema/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>thema</title>
+	<link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+</head>
+<body>
+	<nav class = "navbar navbar-expand-lg navbar-dark bg-dark">
+  		<a class="navbar-brand" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Navbar </font> </font> </a>
+		  <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "# navbarColor02" aria-controls = "navbarColor02" aria-extended = "false" aria-label = "Basculer la navigation ">
+		    <span class = "navbar-toggler-icon"> </span>
+		  </button>
+
+		  <div class = "collapse navbar-collapse" id = "navbarColor02">
+		    <ul class = "navbar-nav mr-auto">
+		      <li class = "nav-item active">
+		        <a class="nav-link" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Maison
+		          </font> </font> <span class = "sr-only"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> (actuel) </ font > </font> </span>
+		        </a>
+		      </li>
+		      <li class = "nav-item">
+		        <a class="nav-link" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Fonctionnalités </font> </font> </a>
+		      </li>
+		      <li class = "nav-item">
+		        <a class="nav-link" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Tarification </font> </font> </a>
+		      </li>
+		      <li class = "nav-item">
+		        <a class="nav-link" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> À propos de </font> </ police> </a>
+		      </li>
+		      <li class = "nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"> <font style = "vertical-align: inherit; "> <font style =" vertical-align: inherit; "> Menu déroulant </font> </font> </a>
+		        <div class = "dropdown-menu">
+		          <a class="dropdown-item" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> action </font> </font> </a>
+		          <a class="dropdown-item" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Une autre action </font> </ police> </a>
+		          <a class="dropdown-item" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Autre choix ici </font> </ police> </a>
+		          <div class = "dropdown-divider"> </div>
+		          <a class="dropdown-item" href="#"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Lien séparé </font> </ font > </a>
+		        </div>
+		      </li>
+		    </ul>
+		    <form class = "form-inline my-2 my-lg-0">
+		      <input class = "form-control mr-sm-2" type = "text" placeholder = "Chercher">
+		      <button class = "btn btn-secondaire my-2 my-sm-0" type = "submit"> <font style = "vertical-align: inherit;"> <font style = "vertical-align: inherit;"> Chercher </font> </font> </button>
+		    </form>
+		  </div>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="liste_blog col-lg-4">
+				<h2 class="text-center">Titre</h2>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="" width="200" class="img-responsive img_blog">
+				<p class="text-justify">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati sapiente, 
+					nam illo quaerat sequi repellat officia ducimus amet dolorem accusamus vero earum debitis
+				 	expedita dolorum animi sit voluptatum totam!
+				</p>
+				<div class="button">
+					<a href="#" class="link_blog btn btn-primary">Détail</a>
+				</div>
+			</div>
+			<div class="liste_blog col-lg-4">
+				<h2 class="text-center">Titre</h2>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="" width="200" class="img-responsive img_blog">
+				<p class="text-justify">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati sapiente, 
+					nam illo quaerat sequi repellat officia ducimus amet dolorem accusamus vero earum debitis
+				 	expedita dolorum animi sit voluptatum totam!
+				</p>
+				<div class="button">
+					<a href="#" class="link_blog btn btn-primary">Détail</a>
+				</div>
+			</div>
+			<div class="liste_blog col-lg-4">
+				<h2 class="text-center">Titre</h2>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="" width="200" class="img-responsive img_blog">
+				<p class="text-justify">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati sapiente, 
+					nam illo quaerat sequi repellat officia ducimus amet dolorem accusamus vero earum debitis
+				 	expedita dolorum animi sit voluptatum totam!
+				</p>
+				<div class="button">
+					<a href="#" class="link_blog btn btn-primary">Détail</a>
+				</div>
+			</div>
+			<div class="liste_blog col-lg-4">
+				<h2 class="text-center">Titre</h2>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="" width="200" class="img-responsive img_blog">
+				<p class="text-justify">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati sapiente, 
+					nam illo quaerat sequi repellat officia ducimus amet dolorem accusamus vero earum debitis
+				 	expedita dolorum animi sit voluptatum totam!
+				</p>
+				<div class="button">
+					<a href="#" class="link_blog btn btn-primary">Détail</a>
+				</div>
+			</div>
+			<div class="liste_blog col-lg-4">
+				<h2 class="text-center">Titre</h2>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="" width="200" class="img-responsive img_blog">
+				<p class="text-justify">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati sapiente, 
+					nam illo quaerat sequi repellat officia ducimus amet dolorem accusamus vero earum debitis
+				 	expedita dolorum animi sit voluptatum totam!
+				</p>
+				<div class="button">
+					<a href="#" class="link_blog btn btn-primary">Détail</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
+	integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
+	integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
+	integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
